c3d1b0cb98b38a594ba5731b582b3099f25f074b
kenziehh <ghaisani.nurani@gmail.com>
2025-01-01 09:53:51 +0700

    `feat(vehicle)` : add migration and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Vehicle;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -37,5 +38,6 @@ class DatabaseSeeder extends Seeder
                 'password' => bcrypt('approver123'),
             ]);
         }
+        Vehicle::factory(50)->create();
     }
 }
